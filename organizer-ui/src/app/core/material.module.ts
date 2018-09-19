2fe21438ff3e313165c822cbf7e478b43ff032bb
JaseG256 <jason_r_gibbs@yahoo.com>
2018-09-19 14:44:20 -0400

    Login() method added to LoginComponent

--- organizer-ui/src/app/core/material.module.ts
+++ organizer-ui/src/app/core/material.module.ts
@@ -1,16 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule, MatToolbarModule, MatInputModule,
-     MatTableModule, MatPaginatorModule, MatSortModule, MatCardModule, MatProgressSpinnerModule } from '@angular/material';
+     MatTableModule, MatPaginatorModule, MatSortModule, MatCardModule, MatProgressSpinnerModule, MatDialog } from '@angular/material';
 
 
 @NgModule({
     imports: [CommonModule, MatButtonModule, MatToolbarModule,
         MatInputModule, MatTableModule, MatPaginatorModule, MatSortModule,
-         MatCardModule, MatProgressSpinnerModule],
+         MatCardModule, MatProgressSpinnerModule, MatDialog],
     exports: [CommonModule, MatButtonModule, MatToolbarModule,
         MatInputModule, MatTableModule, MatPaginatorModule, MatSortModule,
-         MatCardModule, MatProgressSpinnerModule]
+         MatCardModule, MatProgressSpinnerModule, MatDialog]
 })
 export class CustomMaterialModule {
 
--- organizer-ui/src/app/login/login.component.ts
+++ organizer-ui/src/app/login/login.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { MatDialog } from '@angular/material';
+import { AuthService } from '../core/auth.service';
+import { TokenStorage } from '../core/token.storage';
 
 @Component({
   selector: 'app-login',
@@ -11,17 +14,25 @@ export class LoginComponent implements OnInit {
   username: string;
   password: string;
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, public dialog: MatDialog, private authService: AuthService,
+  private token: TokenStorage) { }
 
   ngOnInit() {
   }
 
   login(): void {
-    if (this.username === 'admin' && this.password === 'admin') {
-      this.router.navigate(['users']);
-    } else {
-      alert('Invalid Credentials');
-    }
+    this.authService.attemptAuth(this.username, this.password).subscribe(
+      data => {
+        this.token.saveToken(data.token);
+        this.router.navigate(['user']);
+      }
+    );
+
+    // if (this.username === 'admin' && this.password === 'admin') {
+    //   this.router.navigate(['users']);
+    // } else {
+    //   alert('Invalid Credentials');
+    // }
   }
 
 }
